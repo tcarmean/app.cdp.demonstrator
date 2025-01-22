a4b09cdc42fc232cf055044d074da1ac938c29c3
Tim Carmean <tcarmean@ford.com>
2025-01-22 14:50:32 -0500

    call hello world from module a

--- src/demonstrator.c
+++ src/demonstrator.c
@@ -1,3 +1,12 @@
 # Demonstrator Source
 # Print a message from Module A to the Console
 # Print a message from Module B to the Console
+
+// demonstrator.c
+#include <stdio.h>
+#include "module_a.h"
+
+int main() {
+    hello_world();
+    return 0;
+}
