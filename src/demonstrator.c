56b63b498a209d05e30b864fab8ef6895ce51b6b
Tim Carmean <tcarmean@ford.com>
2025-01-24 09:57:52 -0500

    forgot how comments work in C

--- src/demonstrator.c
+++ src/demonstrator.c
@@ -1,6 +1,6 @@
-# Demonstrator Source
+/* Demonstrator Source
 # Print a message from Module A to the Console
-# Print a message from Module B to the Console
+# Print a message from Module B to the Console*/
 
 // demonstrator.c
 #include <stdio.h>
